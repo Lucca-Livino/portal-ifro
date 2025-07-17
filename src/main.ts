2069a32823b93bc70bd0c21e65e026944d7ed0c6
Lucca <lucca.f.livino@gmail.com>
2025-07-17 02:13:20 -0400

    Finalizando a estilização do site

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,27 @@
 import './style/style.css'
 import './style/rodape.css'
 import './style/cabecalho.css'
+import './style/hamburger.css'
 
+
+const menu = document.getElementById('menu')!
+const hamburger = document.getElementById('hamburger')!
+const burgerIcone = document.getElementById('burger-icone') as HTMLImageElement;
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  hamburger.classList.toggle('active');
+  if (hamburger.classList.contains('active')) {
+    burgerIcone.src = '/x.svg';
+  } else {
+    burgerIcone.src = '/burger-menu.svg';
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    menu.classList.remove('open');
+    hamburger.classList.remove('active');
+    burgerIcone.src = '/burger-menu.svg';
+  }
+});
